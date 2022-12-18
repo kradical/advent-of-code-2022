0145e2d10bc6eb4716ca452c772d584daf58c0fc
Konrad Schultz <dev@konradschultz.com>
2022-12-17 23:27:05 -0800

    day 4 part 2 * _ *

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -5,6 +5,34 @@ use std::{
 
 fn main() {
     part_one();
+    part_two();
+}
+
+fn part_two() {
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut total: i32 = 0;
+    for line in reader.lines() {
+        let line_str = line.unwrap();
+
+        let mut spliterator = line_str.split(",");
+        let first_str = spliterator.next().unwrap();
+        let second_str = spliterator.next().unwrap();
+
+        let first_range = parse_elf(first_str);
+        let second_range = parse_elf(second_str);
+
+        if do_ranges_overlap(first_range, second_range) {
+            total += 1;
+        }
+    }
+
+    println!("part 2: {}", total);
+}
+
+fn do_ranges_overlap(r1: [i32; 2], r2: [i32; 2]) -> bool {
+    r1[0] <= r2[1] && r2[0] <= r1[1]
 }
 
 fn part_one() {
